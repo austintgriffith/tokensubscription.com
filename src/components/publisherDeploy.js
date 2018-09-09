f1ad5dfaf4fe880a00bc70550a1fdb9e789cd8ba
jgerk <jeremygerk@Jeremys-MacBook-Pro-2.local>
2018-09-08 22:27:03 -0600

    adding images and working on demo site

--- src/components/publisherDeploy.js
+++ src/components/publisherDeploy.js
@@ -20,12 +20,16 @@ class PublisherDeploy extends Component {
 
     let url = window.location.origin+"/"+contractAddress
     return (
-      <div className="container">
-        Publisher Deploy Page
+      <div>
+        <h1>Congratulations, your contract is setup</h1>
+        <h3>You you are now ready to start accepting subscriptions!</h3>
+        <p>Follow the instructions below to setup your method for people to start subscribing</p>
         <div>
           {contractAddress} {deployed}
         </div>
         <div>
+          <p>You can put a simple button on your website by copying the html code below.</p>
+          <div className="code"><pre><a href="{url}" target="_blank">Subscribe Now</a></pre></div>
           ~~ url, qr code, embed stuff here ~~
           <pre>{url}</pre>
         </div>
