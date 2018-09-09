4f9dd47695ecd6f52850ad94b0aa63c58c516d3b
jgerk <jeremygerk@Jeremys-MacBook-Pro-2.local>
2018-09-08 20:58:31 -0600

    button styles

--- src/App.js
+++ src/App.js
@@ -260,7 +260,7 @@ class App extends Component {
                 this.setState({mode:"subscriber"})
               }}>
               Create Subscription</button>
-            <span>or</span>
+            <span className="side-pad">or</span>
             <button size="2" onClick={()=>{
                 this.setState({mode:"publisher"})
               }}>
@@ -279,7 +279,7 @@ class App extends Component {
               alert("Please connect and unlock web3 to send tokens.")
             }}>
             Create Subscription</button>
-          <span>or</span>
+          <span className="side-pad">or</span>
           <button size="2" onClick={()=>{
               alert("Please connect and unlock web3 to accept tokens.")
             }}>
