d30054013680e61f8645dafbaa5ba7cffc0dcc59
Hunter Chang <hunter@madwiremedia.com>
2018-09-15 07:33:11 -0600

    adding austin stuff

--- grants/src/components/CreateGrants.js
+++ grants/src/components/CreateGrants.js
@@ -1,9 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Address, Blockie, Scaler } from "dapparatus"
 
-const CreateGrant = () => (
-  <div className="container">
-    <h2>Create Grant</h2>
-  </div>
-)
 
-export default CreateGrant;
+class CreateGrants extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+  handleInput(e,data){
+    console.log("INPUT",e,data)
+    let update = {}
+    update[e.target.name] = e.target.value
+    this.setState(update)
+  }
+  componentDidMount() {}
+  render() {
+    return (
+        <Scaler config={{startZoomAt:800,origin:"50px 50px"}}>
+          <h1 style={{marginTop:50}}>Create Grant</h1>
+
+          <div className="form-field">
+          Title:
+          <input type="text" style={{width:650,fontWeight:'bold'}} name="title" value={this.state.title} onChange={this.handleInput.bind(this)} />
+          </div>
+
+          <div className="form-field">
+          Pitch:
+          <textarea style={{width:650,height:180}}  name="pitch" value={this.state.pitch} onChange={this.handleInput.bind(this)}></textarea>
+          </div>
+
+          <div className="form-field">
+          Contract:
+          <button size="2" style={{marginTop:50}} onClick={()=>{
+              //this.props.deployGrant(toAddress,tokenAddress,tokenAmount,timeType,timeAmount,gasPrice,email)
+            }}>
+            Deploy Grant Contract
+          </button>
+          </div>
+
+          <div className="form-field">
+          Description:
+          <textarea style={{width:650,height:580}}  name="desc" value={this.state.desc} onChange={this.handleInput.bind(this)}></textarea>
+          </div>
+
+          <button size="2" style={{marginTop:50}} onClick={()=>{
+              //this.props.deployGrant(toAddress,tokenAddress,tokenAmount,timeType,timeAmount,gasPrice,email)
+            }}>
+            Save
+          </button>
+
+
+          <div style={{marginTop:90,cursor:"pointer"}} onClick={()=>{
+          //  let hash = soliditySha3
+          //  let signature = await web3.eth.personal.sign(""+subscriptionHash,account)
+          }}>
+          </div>
+        </Scaler>
+    );
+  }
+}
+
+export default CreateGrants;
--- grants/src/components/Home.js
+++ grants/src/components/Home.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
 const Home = () => (
-  <div className="container">
-    <h2>Create Homepage</h2>
+  <div className="container text-center">
+    <h2>ETH Grants</h2>
+    <p>Recurring Ethereum funding via token subscriptions powered by meta transactions</p>
+
+    <div style={{marginTop:200,opacity:0.7,fontSize:15}}>
+        <div>Disclaimer: <span style={{color:"#FFFFFF"}}>We built this in a weekend!</span></div>
+        <div>You should inspect <a style={{color:"#dddddd"}} href={"https://etherscan.io/address/0x49748b0380d9370795CbD6809E52C927072107dC"}>our smart contract</a> before using.</div>
+        <div>100% free and open source! Please <a style={{color:"#dddddd"}} href="https://github.com/austintgriffith/tokensubscription.com">contribute</a>!</div>
+    </div>
   </div>
 )
 
